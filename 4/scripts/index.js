ee45732c2b772b317bb31fd1eba1290ce876fb39
keksobot <keksobot@users.noreply.github.com>
2024-06-02 12:12:36 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2472273-cat-energy-31@8e5784ae0e770b98808d63e11ffabd8040ded667 🚀

--- 4/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var t=document.querySelector(".navigationtoggle"),o=document.querySelector(".navigationlist"),n=document.querySelector(".contactsmap");t.classList.remove("navigationtoggle--nojs");o.classList.remove("navigationlist--nojs");n.classList.remove("contactsmap--nojs");
---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var t=document.querySelector(".navigationtoggle"),o=document.querySelector(".navigationlist"),n=document.querySelector(".contactsmap");t.classList.remove("navigationtoggle--nojs");o.classList.remove("navigationlist--nojs");n.classList.remove("contactsmap--nojs");
