f93a1d706f36af958c59b023b719edd86a5b84a8
VtElizaveta <pogotovkoliza@mail.ru>
2024-06-02 16:10:54 +0400

    Добавляет изменения в папку scripts

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const navButton = document.querySelector('.navigationtoggle');
+const navList = document.querySelector('.navigationlist');
+const map = document.querySelector('.contactsmap');
+
+navButton.classList.remove('navigationtoggle--nojs');
+navList.classList.remove('navigationlist--nojs');
+map.classList.remove('contactsmap--nojs');
